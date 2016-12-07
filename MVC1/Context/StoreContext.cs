24f0c301d53f99c7243dcf99e3c9b28cd4504787
Eduardo Campos <mundotic@eduardocamposjimenez.es>
2016-12-07 08:48:14 +0100

    Primer Commit

---
+++ MVC1/Context/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC1.Context
+{
+    public class StoreContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
